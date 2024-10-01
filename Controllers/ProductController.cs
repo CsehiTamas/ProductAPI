6fec2d0ac10dcc445319bf3fd7a01bdcff51c061
Cs.Tamas <csehit@kkszki.hu>
2024-10-01 14:48:08 +0200

    Get endpoint is ready to use.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using bikicsunáj.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace bikicsunáj.Controllers
 {
@@ -7,10 +9,36 @@ namespace bikicsunáj.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        Connect conn = new Connect();
         [HttpGet]
-        public object Get()
+        public List<Product> Get()
         {
-            return new { message = "Hello World." };
+            List<Product> products = new List<Product>();
+
+            conn.Connection.Open();
+
+            string sql = "SELECT * FROM product";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            do 
+            {
+                var result = new Product
+                {
+                    Id = reader.GetGuid(0),
+                    Name = reader.GetString(1),
+                    Price = reader.GetInt32(2),
+                    CreatedTime = reader.GetDateTime(3)
+
+                };
+
+                products.Add(result);
+            }
+            while (reader.Read());
+
+            conn.Connection.Close();
+
+            return products;
         }
     }
 }
--- obj/Debug/net8.0/bikicsunáj.AssemblyInfo.cs
+++ obj/Debug/net8.0/bikicsunáj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bikicsunáj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f2c75b857891aeb70c559840146fb35334f32f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9843f387c87ed749ffb5aaec95b4d81dd74eae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("bikicsunáj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bikicsunáj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
